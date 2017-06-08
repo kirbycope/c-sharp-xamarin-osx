ba43b119034268d749cfcdf39cdade642c29d4e7
Tim Cope <kirbycope@gmail.com>
2017-06-08 13:12:11 -0600

    Initial commit

---
+++ AutomationFramework/AutomationFramework/TestBase.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using Xamarin.UITest;
+
+namespace AutomationFramework
+{
+    public class TestBase
+    {
+		protected static string deviceIdentifiers;
+		protected static string bundleId = ConfigurationManager.AppSettings["bundleId"];
+
+		/// <summary>
+		/// Sets up the App on the appropriate device
+		/// </summary>
+		/// <param name="deviceIdentifier">The Device Identifier to install the app</param>
+		public void Setup(string deviceIdentifier)
+		{
+			App.app = ConfigureApp
+				.iOS
+				.DeviceIdentifier(deviceIdentifier)
+				.InstalledApp(bundleId)
+				.StartApp();
+		}
+
+		/// <summary>
+		/// Gets each Device Identifier from the App.config
+		/// </summary>
+		/// <returns>Each Device Identifier from the App.config</returns>
+		public static IEnumerable<string> GetDevices()
+		{
+			// Get BaseUrl from App.config
+			if (deviceIdentifiers == null)
+			{
+				deviceIdentifiers = ConfigurationManager.AppSettings["deviceIdentifiers"];
+			}
+			// Split the list of devices
+			string[] deviceIdentifiersArray = deviceIdentifiers.Split(',');
+			// Return each browser
+			foreach (string deviceSerial in deviceIdentifiersArray)
+			{
+				yield return deviceSerial;
+			}
+		}
+    }
+}
---
+++ TimothyCopeIos/TimothyCopeIos/HomePageTests.cs
@@ -0,0 +1,20 @@
+﻿using AutomationFramework;
+using NUnit.Framework;
+
+namespace TimothyCopeIos
+{
+    [TestFixture]
+    public class HomePageTests : TestBase
+    {
+		[Test]
+		[Description("An example test for the guide on TimothyCope.com")]
+		[TestCaseSource(typeof(TestBase), "GetDevices")]
+		public void ExampleTest(string deviceSerial)
+		{
+			// Setup the App on the provided service
+			Setup(deviceSerial);
+			// Verify the Logo is displayed on the "Home" page
+			Assert.IsTrue(App.HomePage.imgLogo.Displayed);
+		}
+    }
+}
